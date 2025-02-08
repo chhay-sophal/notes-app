8a5bc41772a8aa598a754da6e9638ce28042b17e
Sophal Chhay <134204301+chhay-sophal@users.noreply.github.com>
2025-02-08 17:47:48 +0700

    hide login and register button from header for logged out user

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import { RouterView, useRoute } from 'vue-router'
 import NavBar from './components/NavBar.vue'
+
+const route = useRoute()
 </script>
 
 <template>
   <header class="bg-blue-600 text-white p-4 shadow-md">
     <div class="container mx-auto flex justify-between items-center">
       <h1 class="text-2xl font-bold">Notes App</h1>
-      <NavBar />
+      <NavBar v-if="route.name !== 'login' && route.name !== 'register'" />
     </div>
   </header>
 
