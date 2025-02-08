5ed9bc53853988c6b258725bb50504900ffda2da
Sophal Chhay <134204301+chhay-sophal@users.noreply.github.com>
2025-02-08 15:31:36 +0700

    conditionally display the login and register buttons

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -2,21 +2,33 @@
   <nav class="space-x-4">
     <RouterLink to="/" class="hover:underline">Home</RouterLink>
     <RouterLink to="/about" class="hover:underline">About</RouterLink>
-    <RouterLink to="/login" class="hover:underline">Login</RouterLink>
-    <RouterLink to="/register" class="hover:underline">Register</RouterLink>
-    <button @click="logout" class="hover:underline text-red-500">Logout</button>
+    <RouterLink v-if="!isLoggedIn" to="/login" class="hover:underline">Login</RouterLink>
+    <RouterLink v-if="!isLoggedIn" to="/register" class="hover:underline">Register</RouterLink>
+    <button v-if="isLoggedIn" @click="logout" class="hover:underline text-red-500">Logout</button>
   </nav>
 </template>
 
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'
 import { RouterLink, useRouter } from 'vue-router'
 
 const router = useRouter()
+const isLoggedIn = ref(false)
+
+const checkAuthStatus = () => {
+  const token = localStorage.getItem('token')
+  isLoggedIn.value = !!token
+}
 
 const logout = () => {
   if (confirm('Are you sure you want to log out?')) {
     localStorage.removeItem('token')
+    isLoggedIn.value = false
     router.push('/login')
   }
 }
+
+onMounted(() => {
+  checkAuthStatus()
+})
 </script>
